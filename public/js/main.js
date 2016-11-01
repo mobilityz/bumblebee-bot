0afd248030a5034266c908af7c68b9aab57073eb
Maxime Lenne <maxime.lenne@gmail.com>
2016-11-01 19:56:58 +0100

    #1 #{13

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,10 @@ $('#load-close').click(function(){
 	$('#load').addClass('animated slideOutUp');
 });
 
-L.mapbox.accessToken = '';
+L.mapbox.accessToken = process.env.MAP_BOX_KEY;
 var map = L.mapbox.map('map')
     .setView([38.89399, -77.03659], 17);
-L.tileLayer('https://api.mapbox.com/styles/v1/ecotaco/cisg4kbkt006c2xp8kerbz7nn/tiles/256/{z}/{x}/{y}?access_token=').addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/ecotaco/cisg4kbkt006c2xp8kerbz7nn/tiles/256/{z}/{x}/{y}?access_token=' + process.env.MAP_BOX_KEY).addTo(map);
 
 var featureGroup = L.featureGroup().addTo(map);
 var drawControl = new L.Control.Draw({
