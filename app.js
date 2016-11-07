f64786395e33c4c3bf41d02e8e51bc898e20fd9c
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-11-07 17:32:56 +0100

    Use global Promise to stop Warning from mongoose

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var bots = require('./routes/bots');
 
 var app = express();
 
+// setup connection with MongoDB
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/bumblebee');
+mongoose.Promise = global.Promise;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
