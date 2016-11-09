844e438264c284c4a9b48f98fe1d4918f00f09a7
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-11-09 17:26:57 +0100

    Add test for bot.save()

--- test/models/botTest.js
+++ test/models/botTest.js
@@ -1,9 +1,12 @@
 var chai = require('chai');
-var expect = chai.expect; // we are using the "expect" style of Chai
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 var Bot = require('./../../models/bot');
 
-describe('Bot', function() {
-  it('Bot() create new bot object with default value', function() {
+describe('Bot()', function() {
+  it('Create new bot object with default values', function() {
     var newBot = Bot({
       name: 'Toto',
       zone: {
@@ -23,5 +26,53 @@ describe('Bot', function() {
     expect(newBot.http_method).to.equal(undefined);
     expect(newBot.speed).to.equal(30);
     expect(newBot.precision).to.equal(20);
+  });
+});
+
+// Doesn't curently works, fulfilled or rejected are always true ...
+describe('Bot save()', function() {
+  it('Is good', function() {
+    var new_bot = Bot({
+      name: 'Toto',
+      zone: {
+        coordinates: [[8.086, 3.455], [7.546, 5.558]]
+      }
+    });
+    var bot_save = new_bot.save();
+
+    expect(bot_save).to.be.fulfilled;
+    expect(bot_save).to.eventually.be.equal({test: 5});
+  });
+
+  it('Should require name', function() {
+    var new_bot = Bot({
+      zone: {
+        coordinates: [[8.086, 3.455], [7.546, 5.558]]
+      }
+    });
+    var bot_save = new_bot.save();
+
+    expect(bot_save).to.be.rejected;
+  });
+
+  it('Should require coordinates', function() {
+    var new_bot = Bot({
+      name: 'Toto1'
+    });
+    var bot_save = new_bot.save();
+
+    expect(bot_save).to.be.rejected;
+  });
+
+  it('Attributes name has to be unique', function() {
+    var new_bot = Bot({
+      name: 'Toto',
+      zone: {
+        coordinates: [[8.086, 3.455], [7.546, 5.558]]
+      }
+    });
+    var bot_save = new_bot.save();
+
+    expect(bot_save).to.be.rejected;
   });
 });
