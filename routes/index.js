e871aa0d980b3db81dcc8944603c1e5b2127de9c
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-11-15 21:20:29 +0100

    Road to delete all delayed jobs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var kue = require('kue');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.post('/destroy_jobs', function(req, res, next) {
+  kue.Job.rangeByState('delayed', 0, 10000, 'asc', function( err, jobs ) {
+    jobs.forEach(function(job) {
+      job.remove(function() {});
+    });
+    res.send();
+  });
 });
 
 module.exports = router;
